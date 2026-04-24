90f53b7d890e7753a5370a86699a3e6bd536945c
Kaustav.Surai@justice.gov.uk <Kaustav.Surai@justice.gov.uk>
2026-04-24 06:18:37 +0100

    Initial commit

---
+++ run_local.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Fetch DB details"
+DB_JSON=$(cloud-platform decode-secret --secret=hmpps-person-record-rds-instance-output --namespace=hmpps-person-record-dev)
+
+export DB_NAME=$(echo "$DB_JSON" | jq -r '.data.database_name')
+export DB_URL="jdbc:postgresql://localhost:5432/$DB_NAME"
+export DB_USER=$(echo "$DB_JSON" | jq -r '.data.database_username')
+export DB_PASS=$(echo "$DB_JSON" | jq -r '.data.database_password')
+
+sleep 5
+echo "DB variables loaded"
+
+echo "Generate test data"
+export OUT_FILE="src/main/resources/testdata/data.csv"
+./gradlew generateTestData  --args="'$DB_URL' '$DB_USER' '$DB_PASS' '$OUT_FILE'"
+
+echo "Running Gatling..."
+export GATLING_CLIENT_ID="<CLIENT_ID>"
+export GATLING_CLIENT_SECRET='<CLIENT_SECRET>'
+CLIENT_ID=$GATLING_CLIENT_ID CLIENT_SECRET=$GATLING_CLIENT_SECRET ./gradlew gatlingRun -Denv=dev -Dprofile=local
+
+echo "Open Gatling Report"
+open "build/reports/gatling/$(ls -1t build/reports/gatling | head -1)/index.html"
+
+
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/helper/ApiHelper.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.personrecord.helper
+
+
+import io.gatling.javaapi.core.ChainBuilder
+import io.gatling.javaapi.core.CoreDsl.exec
+import io.gatling.javaapi.http.HttpDsl.http
+import io.gatling.javaapi.http.HttpDsl.status
+import uk.gov.justice.digital.hmpps.personrecord.config.AppConfig
+
+object ApiHelper {
+
+  val getPrisoners: ChainBuilder =
+    exec(
+      http("GET Prisoner")
+        .get(AppConfig.uriGetPrisoner)
+        .header("Authorization", "Bearer ${TokenManager.getToken()}")
+        .check(status().shouldBe(200)),
+    )
+
+  val getCrns: ChainBuilder = exec(
+    http("GET Crn")
+      .get(AppConfig.uriGetCrn)
+      .header("Authorization", "Bearer ${TokenManager.getToken()}")
+      .check(status().shouldBe(200)),
+  )
+
+  val getDefendants: ChainBuilder = exec(
+    http("GET Defendant id")
+      .get(AppConfig.uriGetDefendantId)
+      .header("Authorization", "Bearer ${TokenManager.getToken()}")
+      .check(status().shouldBe(200)),
+  )
+}
