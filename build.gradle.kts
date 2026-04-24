44b9a130a879e08afa6d9bce730fda5cff9819aa
Kaustav Surai <Kaustav.Surai@justice.gov.uk>
2026-04-24 13:27:08 +0100

    CPR-1160 Framework setup and add load tests (#1)
    * Initial commit
    
    * temp:added branch to show workflow
    
    * updated README.md
    
    * fix pipeline path
    
    * removed temp changes

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("io.gatling.gradle") version "3.15.0"
+    id("application")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    gatling("org.postgresql:postgresql:42.7.7")
+    implementation("io.gatling.highcharts:gatling-charts-highcharts:3.15.0")
+    implementation("io.netty:netty-codec-http2:4.1.132.Final")
+    implementation("io.netty:netty-handler:4.1.132.Final")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+application{
+    mainClass.set("uk.gov.justice.digital.hmpps.personrecord.helper.CsvGenerator")
+}
+
+tasks.register<JavaExec>("generateTestData") {
+    group = "application"
+    classpath = sourceSets.getByName("gatling").runtimeClasspath
+    mainClass.set("uk.gov.justice.digital.hmpps.personrecord.helper.CsvGenerator")
+}
+
+tasks.register<Exec>("gatlingRunCi") {
+    group = "gatling"
+    description = "Run un-attended in github ci"
+
+    val args = mutableListOf("gatlingRun")
+    args += listOf("--all")
+
+    workingDir = project.rootDir
+    val wrapper = if (org.gradle.internal.os.OperatingSystem.current().isWindows) "gradlew.bat" else "./gradlew"
+    println("[GATLING][Gradle] $wrapper ${args.joinToString(" ")}")
+    commandLine(wrapper, *args.toTypedArray())
+}
