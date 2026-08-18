724dcafe554bf1fa90e9187e89f5fe0dc4842942
David Sharrocks <30368099+SharrocksDA@users.noreply.github.com>
2026-08-18 16:25:25 +0100

    This change ensures that you don't need to copy and paste the client ID or client secret into the run_local.sh file to run the Gatling tests locally. (#44)

--- run_local.sh
+++ run_local.sh
@@ -3,10 +3,13 @@
 echo "Fetch DB details"
 DB_JSON=$(cloud-platform decode-secret --secret=hmpps-person-record-rds-instance-output --namespace=hmpps-person-record-dev)
 
-export DB_NAME=$(echo "$DB_JSON" | jq -r '.data.database_name')
+export DB_NAME
+DB_NAME=$(echo "$DB_JSON" | jq -r '.data.database_name')
 export DB_URL="jdbc:postgresql://localhost:5432/$DB_NAME"
-export DB_USER=$(echo "$DB_JSON" | jq -r '.data.database_username')
-export DB_PASS=$(echo "$DB_JSON" | jq -r '.data.database_password')
+export DB_USER
+DB_USER=$(echo "$DB_JSON" | jq -r '.data.database_username')
+export DB_PASS
+DB_PASS=$(echo "$DB_JSON" | jq -r '.data.database_password')
 
 sleep 5
 echo "DB variables loaded"
@@ -15,9 +18,17 @@ echo "Generate test data"
 export OUT_FILE="src/main/resources/testdata/data.csv"
 ./gradlew generateTestData  --args="'$DB_URL' '$DB_USER' '$DB_PASS' '$OUT_FILE'"
 
+echo "Fetch Gatling client credentials"
+GATLING_ID_JSON=$(cloud-platform decode-secret --secret=hmpps-person-record-gatling-client-id --namespace=hmpps-person-record-dev)
+GATLING_SECRET_JSON=$(cloud-platform decode-secret --secret=hmpps-person-record-gatling-client-secret --namespace=hmpps-person-record-dev)
+
+export GATLING_CLIENT_ID
+GATLING_CLIENT_ID=$(echo "$GATLING_ID_JSON" | jq -r '.data.id')
+export GATLING_CLIENT_SECRET
+GATLING_CLIENT_SECRET=$(echo "$GATLING_SECRET_JSON" | jq -r '.data.secret')
+echo "Gatling client credentials loaded"
+
 echo "Running Gatling..."
-export GATLING_CLIENT_ID="<CLIENT_ID>"
-export GATLING_CLIENT_SECRET='<CLIENT_SECRET>'
 CLIENT_ID=$GATLING_CLIENT_ID CLIENT_SECRET=$GATLING_CLIENT_SECRET ./gradlew gatlingRun -Denv=dev -DgetPrisonNumber=1 -DgetCrnNumber=1 -DgetDefendantId=1 -Dduration=60
 
 echo "Open Gatling Report"
